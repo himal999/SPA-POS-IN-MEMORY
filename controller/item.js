03e42f71e75b7d62c71c6c871c78bf1f3e9b5cc5
himal999 <himal.official99@gmail.com>
2022-02-24 16:28:41 +0530

    validation update

--- controller/item.js
+++ controller/item.js
@@ -20,39 +20,120 @@ $('#form-clear').click(function(){
 //Validation
 
 var regItemCode = /^(I-)[0-9]{3,4}$/;
-var regItemName = /^[A-z,0-9]{4,100}$/
+var regItemName = /^[A-z,0-9,-]{4,100}$/
 var regItemQty = /^[0-9]{1,}$/
 var regItemPrice = /^[0-9]{1,}(.)[0-9]{1,2}$/
 
-$('#inItemCode').keyup(function(){
-    if(regItemCode.test($('#inItemCode').val())){
-        $('#inItemCode').css("border","1px solid  green")
-    }else{
-        $('#inItemCode').css("border","1px solid red")
-    }
+
+
+$('#inItemCode').on('keyup',function(){
+    btnStatus();
 })
 
-$('#inItemName').keyup(function(){
-    if(regItemName.test($('#inItemName').val())){
-        $('#inItemName').css("border","1px solid  green")
-    }else{
-        $('#inItemName').css("border","1px solid red")
-    }
+$('#inItemName').on('keyup',function(){
+    btnStatus();
 })
-$('#inItemQty').keyup(function(){
-    if(regItemQty.test($('#inItemQty').val())){
-        $('#inItemQty').css("border","1px solid  green")
-    }else{
-        $('#inItemQty').css("border","1px solid red")
-    }
+
+$('#inItemQty').on('keyup',function(){
+    btnStatus();
 })
-$('#inItemPrice').keyup(function(){
-    if(regItemPrice.test($('#inItemPrice').val())){
-        $('#inItemPrice').css("border","1px solid  green")
-    }else{
-        $('#inItemPrice').css("border","1px solid red")
-    }
+$('#inItemPrice').on('keyup',function(){
+    btnStatus();
 })
+
+
+
+
+
+
+
+function btnStatus(){
+    var b = valid(); 
+
+    if(b){
+        $("#form-add").removeAttr("disabled")
+    }else{
+        $("#form-add").attr("disabled",'true')
+    }
+}
+
+function valid(){
+    if( regItemCode.test($('#inItemCode').val())){
+        $('#inItemCode').css("border","1px solid  green");
+        $('#errorCode').text("");
+        $('inItemName').focus();
+        if(regItemName.test($('#inItemName').val())){
+            $('#inItemName').css("border","1px solid  green")
+            $('#errorName').text("")
+            $('inItemQty').focus();
+            if(regItemQty.test($('#inItemQty').val())){
+                $('#inItemQty').css("border","1px solid  green")
+                $('#errorQty').text("")
+                $('inItemPrice').focus();
+                if(regItemPrice.test($('#inItemPrice').val())){
+                    $('#inItemPrice').css("border","1px solid  green")
+                    $('#errorPrice').text("")
+                    return true;
+                }else{
+                    $('#inItemPrice').css("border","1px solid red")
+                    $('#errorPrice').text("Please enter .x or .xx ")
+                    $('#inItemPrice').focus();
+                    return false;
+                }
+            }else{
+                $('#inItemQty').css("border","1px solid red")
+                $('#errorQty').text("Please enter valid Item Qty")
+                $('#inItemQty').focus();
+                return false;
+            }
+        }else{
+            $('#inItemName').css("border","1px solid red")
+            $('#errorName').text("Please don't using @ , / , \ , ' , ; , . , $ , % ,# and etc")
+            $('#inItemName').focus();
+            return false;
+        }
+    }else{
+        $('#inItemCode').css("border","1px solid red")
+        $('#errorCode').text("Please enter valid Item Code")
+        $('#inItemCode').focus();
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
