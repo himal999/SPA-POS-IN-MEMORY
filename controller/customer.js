ffb87264e3fe9b171ed82c4a5ff594770d79608d
himal999 <himal.official99@gmail.com>
2022-02-28 08:07:35 +0530

    add function step-1

--- controller/customer.js
+++ controller/customer.js
@@ -3,6 +3,33 @@ $("document").ready(function(){
 })
 
 
+//add customer
+
+$("#btnAddCustomer").click(function(){
+   $("#form-header-customer").text("Add New Customer");
+   enable();
+   $("#inCustomerNic").focus();
+   $("#form-clear-customer").removeAttr("disabled")
+});
+
+$("#form-clear-customer").click(function(){
+    clearField();
+})
+
+$("#inCustomerNic").keyup(function(){
+    btnStatus();
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 //extra methods
@@ -13,6 +40,8 @@ var disable = function(){
     $("#inCustomerName").attr("disabled","disabled")
     $("#inCustomerAddress").attr("disabled","disabled")
     $("#inCustomerSalary").attr("disabled","disabled")
+    $("#form-clear-customer").attr("disabled","disabled")
+    $("#saveData-customer").attr("disabled","disabled")
 }
 
 var enable = function(){
@@ -21,4 +50,31 @@ var enable = function(){
     $("#inCustomerName").removeAttr("disabled")
     $("#inCustomerAddress").removeAttr("disabled")
     $("#inCustomerSalary").removeAttr("disabled")
+}
+
+//clear all field
+
+var clearField = function(){
+    $("#inCustomerNic").val('')
+    $("#inCustomerName").val('')
+    $("#inCustomerAddress").val('')
+    $("#inCustomerSalary").val('')
+}
+
+
+//btn disble or enable check
+
+var btnStatus = function(){
+    if(valid){
+        $("#saveData-customer").removeAttr("disabled")
+    }else{
+        $("#saveData-customer").attr('disabled',"disabled")
+    }
+}
+
+// validation
+
+
+var valid = function(){
+    if("")
 }
